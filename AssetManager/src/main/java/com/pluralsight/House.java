021d3fb939a7bea2619ebebef6ae1c70417a1816
mattchristenson <matt@rcc.team>
2024-10-30 12:34:28 -0500

    after demo

--- AssetManager/src/main/java/com/pluralsight/House.java
+++ AssetManager/src/main/java/com/pluralsight/House.java
@@ -2,8 +2,66 @@ package com.pluralsight;
 
 public class House extends Asset{
 
+    private String address;
+    private int condition; // 1 - excellent, 2 - good, 3 - fair, 4 - poor
+    private int squareFoot;
+    private int lotSize;
 
-    public House(String description, String dateAcquired, double originalCost) {
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
         super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public double getValue() {
+        double value = 0.0;
+        if (this.condition ==1){
+            value = (180.00 * squareFoot) + (.25 * lotSize);
+        }else if (this.condition ==2){
+            value = (130.00 * squareFoot) + (.25 * lotSize);
+        }else if (this.condition ==3){
+            value = (90.00 * squareFoot) + (.25 * lotSize);
+        }else if (this.condition == 4){
+            value = (80.00 * squareFoot) + (.25 * lotSize);
+        }
+        return value;
+
+
     }
 }
--- AssetManager/src/main/java/com/pluralsight/Main.java
+++ AssetManager/src/main/java/com/pluralsight/Main.java
@@ -8,18 +8,30 @@ public class Main {
 
         ArrayList<Asset> assets = new ArrayList<>();
 
-        Asset a1 = new Asset("Golden Necklace", "2004", 200);
+        //Asset a1 = new Asset("Golden Necklace", "2004", 200);
 
-        assets.add(a1);
+        //assets.add(a1);
 
-      //  House h1 = new House();
-      //  House h2 = new House();
-      //  Vehicle v1 = new Vehicle();
-      //  Vehicle v2 = new Vehicle();
+        House h1 = new House("My main house", "2001", 200000, "123 S Street.", 2, 1500, 40000);
 
+        House h2 = new House("My vacation house", "2019", 150000, "567 N Street.", 3, 900, 15000);
+
+        assets.add(h1);
+        assets.add(h2);
+
+        Vehicle v1 = new Vehicle("Daily Driver", "2011", 40000, "Jeep Liberty", 2011, 250000);
+        Vehicle v2 = new Vehicle("Garage Classic", "2000", 15000, "Jeep CJ-8", 1981, 50000);
+
+        assets.add(v1);
+        assets.add(v2);
+
+        double networth = 0;
         for(Asset a : assets){
             System.out.println(a.getDescription() + " Acquired on " + a.getDateAcquired() + " for " + a.getOriginalCost() + " is now worth: " + a.getValue());
+            networth += a.getValue();
         }
+
+        System.out.println("Total net worth:" + networth);
     }
 
 
